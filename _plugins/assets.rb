e4d50b60d27cade8f08c5dd9d4e427c26c26a9f9
Bradley Wright <brad@intranation.com>
2012-09-30 23:10:16 +0100

    Make assets to convert configurable

--- _plugins/assets.rb
+++ _plugins/assets.rb
@@ -11,13 +11,14 @@ module Jekyll
         env.logger = Logger.new(STDOUT)
       end
 
-      assets.append_path(File.join(root, site.config['sprockets_assets']))
-      assets.append_path(File.join(root, site.config['sprockets_assets'], "css"))
-      assets.append_path(File.join(root, site.config['sprockets_assets'], "js"))
+      assets.append_path(File.join(root, site.config['sprockets_root']))
+      assets.append_path(File.join(root, site.config['sprockets_root'], "css"))
+      assets.append_path(File.join(root, site.config['sprockets_root'], "js"))
 
       target_dir = Pathname(root).join(site.config['sprockets_target'])
+      to_convert = site.config['sprockets_assets'].split(' ')
 
-      %w( main.css ).each do |asset_name|
+      to_convert.each do |asset_name|
         target_file = target_dir.join(asset_name)
         asset = assets.find_asset(asset_name)
         asset.write_to(target_file)
