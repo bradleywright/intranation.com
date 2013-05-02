6ae077d0f032d1e66f47c232b3da646294a4e082
Bradley Wright <brad@intranation.com>
2013-05-02 22:24:47 +0100

    Update Gemfile and config for 1.0rc

--- _plugins/assets.rb
+++ _plugins/assets.rb
@@ -8,7 +8,7 @@ module Jekyll
     def generate(site)
       root = site.source
       assets = Sprockets::Environment.new(root) do |env|
-        env.logger = Logger.new(STDOUT)
+
       end
 
       assets.append_path(File.join(root, site.config['sprockets_root']))
